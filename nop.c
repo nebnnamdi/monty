96475adf6c698ce1c3b8046d7b68f73a3a8abd5d
Nebeolisa Nnamdi <nebnnamdi@gmail.com>
2022-09-07 07:30:18 -0700

    monty task

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ *nop - function to do nothing
+ *@stack: pointer to to pointer to doubly linked list
+ *@line_number: number of lines in file
+ *Return: nothing
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+(void) stack;
+(void) line_number;
+return;
+}
